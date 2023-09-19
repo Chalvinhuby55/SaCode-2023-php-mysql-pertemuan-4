a6b6cd4b96cd50178166fab71aeb6bc28731cbb5
Chalvinhuby55 <cjemihuby@gmail.com>
2023-09-19 19:09:45 +0900

    Tipe data Float

---
+++ float.php
@@ -0,0 +1,22 @@
+<?php
+$x = 10.6;
+$y = 77.7;
+
+$tambah = $x + $y;
+var_dump($tambah);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TIPE DATA FLOAT</title>
+</head>
+<body>
+    <p>Tipe Data Float (bilangan pecahan)</p>
+    <P><?=$x?></P>
+    <P><?=$y?></P>
+    <P><?=$x?> + <?=$y?> = <?=$tambah?></P>
+    
+</body>
+</html>
